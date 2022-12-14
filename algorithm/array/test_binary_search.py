555b4486d3ec4f379c196b9fc3dcb7b2713369c5
Zexi Jesse Zhuang <JesseZhuang@users.noreply.github.com>
2022-12-14 03:46:07 +0000

    rename and add test

--- algorithm/array/test_binary_search.py
+++ algorithm/array/test_binary_search.py
@@ -1,3 +1,4 @@
+# pylint: disable=missing-function-docstring
 '''binary search in python'''
 import unittest
 from bisect import bisect_left
@@ -9,8 +10,10 @@ class TestBinarySearch(unittest.TestCase):
     list1 = [0, 1, 2, 3, 4, 6]
 
     def test_bisect_left_not_found(self):
-        '''binary search left'''
         self.assertEqual(5, bisect_left(self.__class__.list1, 5))
+
+    def test_bisect_left_existing(self):
+        self.assertEqual(4, bisect_left(self.__class__.list1, 4))
 
 
 if __name__ == '__main__':
