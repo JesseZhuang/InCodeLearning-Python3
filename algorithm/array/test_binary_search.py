5ce3521941004847698f50832335bba5192593d1
Zexi Jesse Zhuang <JesseZhuang@users.noreply.github.com>
2022-12-14 04:06:41 +0000

    binary search add test

--- algorithm/array/test_binary_search.py
+++ algorithm/array/test_binary_search.py
@@ -1,7 +1,7 @@
 # pylint: disable=missing-function-docstring
 '''binary search in python'''
 import unittest
-from bisect import bisect_left
+from bisect import bisect_left, bisect_right
 
 
 class TestBinarySearch(unittest.TestCase):
@@ -14,6 +14,18 @@ class TestBinarySearch(unittest.TestCase):
 
     def test_bisect_left_existing(self):
         self.assertEqual(4, bisect_left(self.__class__.list1, 4))
+
+    def test_bisect_left_existing_duplicate(self):
+        self.assertEqual(1, bisect_left([0, 1, 1, 1], 1))
+
+    def test_bisect_left_found(self):
+        self.assertEqual(2, bisect_left(self.__class__.list1, 2))
+
+    def test_bisect_right_found(self):
+        self.assertEqual(3, bisect_right(self.__class__.list1, 2))
+
+    def test_bisect_right_existing(self):
+        self.assertEqual(4, bisect_right([0, 1, 1, 1], 1))
 
 
 if __name__ == '__main__':
