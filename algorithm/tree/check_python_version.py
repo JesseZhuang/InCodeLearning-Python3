88a36c68dee69cf45e2ead2b6598b1bd55f953dd
Jesse Zhuang <zhzexi@gmail.com>
2022-12-22 22:12:43 -0800

    check py version

---
+++ algorithm/tree/check_python_version.py
@@ -0,0 +1,5 @@
+'''check python version in coding environments'''
+from sys import version
+
+print("python version:", version)
+# python version: 3.11.0 (main, Oct 25 2022, 14:13:24) [Clang 14.0.0 (clang-1400.0.29.202)]
