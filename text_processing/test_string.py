7c79e1fa5a670a042121770c19886829ef184e86
Jesse Zhuang <zhzexi@gmail.com>
2023-01-06 22:11:16 -0800

    python strings, official tutorial 3.1.2

---
+++ text_processing/test_string.py
@@ -0,0 +1,53 @@
+'''strings'''
+
+import os
+import platform
+import unittest
+
+
+class TestString(unittest.TestCase):
+    '''python strings'''
+
+    def test_raw_string(self):
+        '''raw string, no escape'''
+        print(r'C:\path\name')  # run this file or debug button in test explorer in vs code
+        # raw string cannot end with back slash, e.g., r'C:\name\'
+        self.assertEqual(r'C:\path\name', 'C:\\path\\name')
+        path = os.path.join(r'C:\this\will\work', '')
+        if platform.uname()[0] == 'Darwin':
+            self.assertEqual('C:\\this\\will\\work/', path)
+
+    def test_multi_line(self):
+        '''multiline string'''
+        print(  # back slash in doc string omits new line
+            '''
+        usage: thingy [options]
+            -h\
+            display help message
+            -H hostname     hostname to connect to
+        ''')
+        ms1 = 'multi-lines\nstring\n1\n'
+        self.assertEqual(  # ugly: identation will be considered as white spaces
+            ms1,
+            '''\
+multi-lines
+string
+1
+'''
+        )
+        ms2 = ('multi-lines\n'
+               'string\n'
+               '1\n')
+        self.assertEqual(ms1, ms2)
+
+    def test_immutable(self):
+        '''
+        python strings are immutable
+        no char type in python, see bytes type
+        '''
+        with self.assertRaises(TypeError):
+            'word'[0] = 'c'
+
+
+if __name__ == '__main__':
+    unittest.main()
