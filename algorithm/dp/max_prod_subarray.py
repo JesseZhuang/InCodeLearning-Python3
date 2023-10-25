c8c778f532a3ad078f1fe887d4e447bfec233e92
Jesse Zhuang <zhzexi@gmail.com>
2023-10-25 14:17:49 -0700

    lc 152

---
+++ algorithm/dp/max_prod_subarray.py
@@ -0,0 +1,19 @@
+'''leet code 152 '''
+
+
+import math
+from typing import List
+
+
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        '''O(n) time, O(1) space. 88ms, 16.8MB'''
+        max_here, min_here = 1, 1
+        res = -math.inf
+        for n in nums:
+            # important to calc two products before update
+            p1, p2 = max_here*n, min_here*n
+            max_here = max(n, p1, p2)
+            min_here = min(n, p1, p2)
+            res = max(max_here, res)  # missed res, no need min
+        return res
