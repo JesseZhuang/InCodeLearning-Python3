5af2be916ae764f86cc214be707d144dfe886bf2
Jesse Zhuang <zhzexi@gmail.com>
2024-03-25 16:58:12 -0700

    lc practice 213

--- algorithm/dp/house_robber_ii.py
+++ algorithm/dp/house_robber_ii.py
@@ -1,17 +1,18 @@
-'''leetcode 213'''
+'''leet code 213 medium'''
 
 from typing import List
 
 
-def rob(nums: List[int]) -> int:
-    '''dp O(n) time, O(1) space, 42ms, 13.8Mb'''
-    def simple_rob(nums, i, j) -> int:
-        robbed, did_not_rob_prev = 0, 0
-        for idx in range(i, j):
-            robbed, did_not_rob_prev = did_not_rob_prev + nums[idx], max(did_not_rob_prev, robbed)
-        return max(robbed, did_not_rob_prev)
-    if not nums:
-        return 0
-    if len(nums) == 1:
-        return nums[0]
-    return max(simple_rob(nums, 0, len(nums) - 1), simple_rob(nums, 1, len(nums)))
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        def simple_rob(nums, l, r):
+            did_not, robbed = 0, 0
+            for i in range(l, r):
+                t = did_not+nums[i]
+                did_not = max(did_not, robbed)
+                robbed = t
+            return max(robbed, did_not)
+        if len(nums) == 1:
+            return nums[0]
+        else:
+            return max(simple_rob(nums, 0, len(nums)-1), simple_rob(nums, 1, len(nums)))
