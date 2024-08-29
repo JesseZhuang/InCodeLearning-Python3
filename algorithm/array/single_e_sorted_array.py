dccaade9e8ae8ee4e272d8b436b09dbd4503ed15
Jesse Zhuang <zhzexi@gmail.com>
2024-08-29 11:18:09 -0700

    lc 540

---
+++ algorithm/array/single_e_sorted_array.py
@@ -0,0 +1,15 @@
+'''leet code 540, medium'''
+
+from typing import List
+
+
+class Solution:
+    def singleNonDuplicate(self, nums: List[int]) -> int:
+        l, r = 0, len(nums) - 1
+        while l < r:
+            mid = l + (r - l) // 2
+            if nums[mid] == nums[mid ^ 1]:
+                l = mid + 1
+            else:
+                r = mid
+        return nums[l]
