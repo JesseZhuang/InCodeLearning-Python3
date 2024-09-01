d7ec5686a372cfb3a122f2f53a370c61d174e597
Jesse Zhuang <zhzexi@gmail.com>
2024-09-01 15:27:02 -0700

    lc 767

---
+++ algorithm/heap/reorganize_string.py
@@ -0,0 +1,37 @@
+"""leet code 767, medium"""
+
+import heapq
+from collections import Counter
+
+
+class Solution1:
+    def reorganizeString(self, s: str) -> str:
+        counter = Counter(s)
+        char_cnt = [(-counter[c], c) for c in counter]
+        heapq.heapify(char_cnt)
+
+        res, prev = [], None
+        while char_cnt:
+            _, c = heapq.heappop(char_cnt)
+            res.append(c)
+            counter[c] -= 1
+            if prev and counter[prev] > 0: heapq.heappush(char_cnt, (-counter[prev], prev))
+            prev = c
+        if counter[prev] == 0:
+            return "".join(res)
+        else:
+            return ""
+
+
+class Solution2:
+    def reorganizeString(self, s: str) -> str:
+        counts = Counter(s).most_common()
+        n = len(s)
+        if counts[0][1] > (n + 1) // 2: return ""
+        res, i = [''] * n, 0
+        for c, cnt in counts:
+            for j in range(cnt):
+                res[i] = c
+                i += 2
+                if i >= n: i = 1
+        return "".join(res)
