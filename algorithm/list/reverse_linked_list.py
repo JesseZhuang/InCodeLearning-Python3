d62dfa290501d1c00385703666b464adb53802e2
Jesse Zhuang <zhzexi@gmail.com>
2024-10-28 21:46:00 -0700

    lc practice

---
+++ algorithm/list/reverse_linked_list.py
@@ -0,0 +1,25 @@
+"""leet code 206, easy"""
+from typing import Optional
+
+from algorithm.struct.list_node import ListNode
+
+
+class Solution1:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        res = None
+        while head:  # revise head.next (tmp to res), advance res and head
+            tmp = head.next
+            head.next = res
+            res = head
+            head = tmp  # head point to None at the end
+        return res
+
+
+class Solution2:
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None or head.next is None:
+            return head
+        new_head = self.reverseList(head.next)  # get the new head, then reverse the last two links
+        head.next.next = head  # reverse head.next.next
+        head.next = None  # reverse head.next
+        return new_head
