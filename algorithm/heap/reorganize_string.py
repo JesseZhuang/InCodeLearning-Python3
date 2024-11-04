3a486ce93cd9bdc5ebaf93194894e60a1708e226
Jesse Zhuang <zhzexi@gmail.com>
2024-11-03 22:28:45 -0800

    lc practice

--- algorithm/heap/reorganize_string.py
+++ algorithm/heap/reorganize_string.py
@@ -1,10 +1,51 @@
 """leet code 767, medium"""
 
 import heapq
-from collections import Counter
+from collections import Counter, defaultdict
 
 
-class Solution1:
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        counts = defaultdict(int)
+        max_c, max_cnt = None, 0
+        for c in s:
+            counts[c] += 1
+            if counts[c] > max_cnt:
+                max_c, max_cnt = c, counts[c]
+        if max_cnt > (len(s) + 1) // 2:
+            return ''
+        i = 0
+        res = [''] * len(s)
+        while counts[max_c] > 0:
+            res[i] = max_c
+            i += 2
+            counts[max_c] -= 1
+        for c in counts:
+            while counts[c] > 0:
+                if i > len(s) - 1:
+                    i = 1
+                res[i] = c
+                i += 2
+                counts[c] -= 1
+
+        return ''.join(res)
+
+
+class Solution2:
+    def reorganizeString(self, s: str) -> str:
+        counts = Counter(s).most_common()  # sorting in most_common()
+        n = len(s)
+        if counts[0][1] > (n + 1) // 2: return ""
+        res, i = [""] * n, 0
+        for c, cnt in counts:
+            for j in range(cnt):
+                res[i] = c
+                i += 2
+                if i >= n: i = 1
+        return "".join(res)
+
+
+class Solution3:
     def reorganizeString(self, s: str) -> str:
         """36ms, 16.62mb"""
         counter = Counter(s)
@@ -19,18 +60,4 @@ class Solution1:
             counter[c] -= 1
             if prev and counter[prev] > 0: heapq.heappush(char_cnt, (-counter[prev], prev))
             prev = c
-        return "".join(res)
-
-
-class Solution2:
-    def reorganizeString(self, s: str) -> str:
-        counts = Counter(s).most_common()  # sorting in most_common()
-        n = len(s)
-        if counts[0][1] > (n + 1) // 2: return ""
-        res, i = [""] * n, 0
-        for c, cnt in counts:
-            for j in range(cnt):
-                res[i] = c
-                i += 2
-                if i >= n: i = 1
         return "".join(res)
